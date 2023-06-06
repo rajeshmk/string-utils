962aacfbdcd23474830077afbb9716cee1809bfd
Raju Koyilandy <mkrajesh.kdy@gmail.com>
2023-06-06 22:04:34 +0530

    minimum length of unique string

--- src/Encoder.php
+++ src/Encoder.php
@@ -2,6 +2,8 @@
 
 namespace CodeArtery\String;
 
+use RuntimeException;
+
 class Encoder
 {
     public static function uniqueHash(int $length = 40)
@@ -11,8 +13,11 @@ class Encoder
 
         $base36 = strrev(base_convert($hex, 16, 36)).base_convert($decimal, 10, 36);
 
-        // Ensure minimum length of 32 characters
-        $pad_length = max(32, $length) - strlen($base36);
+        if ($length < 24) {
+            throw new RuntimeException('Minimum length must be 24 characters.');
+        }
+
+        $pad_length = $length - strlen($base36);
 
         $pad_string = '';
         while (($len = strlen($pad_string)) < $pad_length) {
